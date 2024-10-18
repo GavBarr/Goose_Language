8e120aed894c9d3082900a51f428c1c221b1f0c0
Gavin Barrett <107496821+GavBarr@users.noreply.github.com>
2024-10-17 20:54:46 -0600

    Add files via upload

---
+++ ELMNT_language/lexer/lexer.py
@@ -0,0 +1,24 @@
+#Lexer Class
+#Author: Gavin Barrett
+import re
+
+
+
+class Lexer():
+
+
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __repr__(self):
+        return f"Token({self.type}, {self.value})"
+    
+
+    def checkOperator(input):
+        matching = re.match()
+
+    def tokenizeString(input):
+
+        pass
+        
