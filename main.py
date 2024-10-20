f3efdf7cf7aaaf531406e7b77ba221af355f0f82
Gavin Barrett <gavinbarrett2001@gmail.com>
2024-10-20 10:47:16 -0600

    new AST parser and added evaluate() to evaluate the tree node values etc.

--- main.py
+++ main.py
@@ -2,12 +2,31 @@ from lexer.lexer import tokenizeCode
 from parser.parser import Parser
 
 
+def evaluate(node):
+    #print(node)
+    if node[0] == 'NUMBER': #check for the type and then return the value of the NUMBER node
+        print('found')
+        return float(node[1]) #returning the value of the LEFT or RIGHT NUMBER node
+    elif node[0] == 'Operator':
+        op = node[1]
+        left = evaluate(node[2])
+        right = evaluate(node[3])
+        if op == '+':
+            return left + right
+        elif op == '-':
+            return left - right
+        elif op == '*':
+            return left * right
+        elif op == '/':
+            return left / right
+
+
 code = '''
-        1+1
+        1+1*10
         '''
 
 tokens = tokenizeCode(code)
-#for token in tokens:
+#efor token in tokens:
    # print(token)
 
 #print(type(tokens))
@@ -16,3 +35,5 @@ token_list = iter(tokens) #make the token_list an iterator so then I can use the
 p = Parser(token_list)
 result = p.parseExpression()
 #print(result)
+eval = evaluate(result)
+print(eval)
--- parser/parser.py
+++ parser/parser.py
@@ -23,79 +23,56 @@ class Parser:
         self.current_token = next(self.token_list, None)
 
 
-    #Basically just going to the next token in the list and then processing it
-    def eat(self, token_type):
-        if self.current_token.type == token_type:
+    #I want to grab the current type of the token being processed and then go to the next token of from the token list
+    def consumeToken(self, token_type):
+        if self.current_token and self.current_token.type == token_type:
             self.current_token = next(self.token_list, None)
-            #print(f"Current Token:{self.current_token}")
-        else:
-            raise SyntaxError(f"Expected {token_type}")
 
+    
 
+    #looking for any + or - operators, to set as the root of the tree
     def parseExpression(self):
-        #Looking for an operator of plus or minus of two TERMS
-        print(f"Expression Current Token {self.current_token}\n")
-        node = self.parseTerm() # this is finding the first term of the expression, to basically initialize the tree etc
-        print(f'2nd Expression Current Token {self.current_token}/n')
-        while self.current_token and self.current_token.type in ('MINUS','ADD'):# we need to check for self.current_token, to see if there is a valid token or not
-            token = self.current_token
-            operator_node = token
-            self.eat(operator_node.type)
-            print(f"Before Expression Node Found Token {self.current_token}")
-            node = ('operator', operator_node.value, node, self.parseTerm()) #the last two tuple fields, are the left and right nodes connected to the node
-            print(f"After Expression Node Found Token {self.current_token}\n")
-            #                                        left  right
-            #print(node[1])
+        #initialize by looking for the potential left child node, which would could be a Term or Factor, needs to look at 
+        #parseTerm() first becuase of the syntax heirarchy
+        node = self.parseTerm()
+        #while there is a valid token and a valid type of + or - we will evaluate the token and assign a node
+        while self.current_token and self.current_token.type in ('MINUS','ADD'):
+            
+            #set the operator node to the current node, becuse the type matches + or -
+            operator = self.current_token
+
+            #we want to then consume the token and move to the next in the list
+            self.consumeToken(operator.type)
+
+            #assign the node as a tuple, with the type, value, left child and right child
+            node = ('Operator', operator.value, node, self.parseTerm())#node inside of the tuple, is the node from looking at self.parseTerm()
+            #                                       left  right
+        print(f'Node {node}')
         return node
 
-            
-
-
+    #very similar to the parseExpression, but now we are looking at and comparing for the next node as a Factor rather than a Term
     def parseTerm(self):
-        print(f"Term Current Token {self.current_token}\n")
+        #look for the Factor as the node
         node = self.parseFactor()
-        print(f"2nd Term Current Token {self.current_token}")
-       # print(self.current_token.type)
-        
+
+        #check for a different operator, in this case for any multiplication or division. if nothing is found, then only return the factor valule as a node
         while self.current_token and self.current_token.type in ('MULTIPLY','DIVIDE'):
-            token = self.current_token
-            operator_node = token
-            self.eat(operator_node.type)
-            node = ('operatory', operator_node.value, node, self.parseFactor())
-            #print(f"Term Node {node}")
+            operator = self.current_token
+            self.consumeToken(operator.type)
+
+            node = ('Operator', operator.value, node, self.parseFactor())
+
         return node
 
+    
     def parseFactor(self):
-        print(f"Factor Current Token {self.current_token}\n")
-        token = self.current_token
+        
         if self.current_token and self.current_token.type == "INTEGER_LITERAL":
-            self.eat("INTEGER_LITERAL")
+            token = ("NUMBER", self.current_token.value)#we store the type and the value as a token and then proceed to the next token
+            self.consumeToken(self.current_token.type)
+            return token
+        #else if check for parantheses
+        #else:
+            #raise SyntaxError(f"Syntax Issue on line {self.current_token.line}")
 
-            return ("INTEGER_LITERAL", token.value) #return the value so then it can be stored into the tree/node correctly
-        else:
-            return "Syntax Error"
-
-            
-
-
-class NumberNode:
-    def __init__(self):
-        self.type_ = type_
-        self.value = value
-        self.line = line
-        self.column = column
-
-
-class OperatorNode:
-    def __init__(self):
-        self.type_ = type_
-        self.value = value
-        self.line = line
-        self.column = column
-
-class ExpressionNode:
-    def __init__(self):
-        self.type_ = type_
-        self.value = value
-        self.line = line
-        self.column = column
+        
